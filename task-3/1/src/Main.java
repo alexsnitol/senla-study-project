50dab83ae67904a16c18d213da1f5f57be601a39
alexsnitol <sslotin74@gmail.com>
2022-03-21 11:43:41 +0300

    renamed variables from letters to words replaced duplicate values to constants replaced duplicate operations to methods edited sum numbers on concatenation

--- task-3/1/src/Main.java
+++ task-3/1/src/Main.java
@@ -1,16 +1,33 @@
 import java.util.Random;
 
+import static java.lang.System.out;
+
 // program 3
 public class Main {
-    public static void main(String[] args) {
-        int a = new Random().nextInt(899)+100;
-        int b = new Random().nextInt(899)+100;
-        int c = new Random().nextInt(899)+100;
-        int d = a*1000 + b/100*100 + b%100/10*10 + b%10;
+    public static final int MAX_INT = 999;
+    public static final int SHIFT = 100;
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d + " - " + c + " = " + (d - c));
+    public static int concatenateNumbers(Integer num1, Integer num2) {
+        String concatenateNumbers = num1.toString() + num2.toString();
+        return Integer.parseInt(concatenateNumbers);
+    }
+
+    public static int getRandomNumber(int from, int to) {
+        if (to >= from)
+            return new Random().nextInt(to - from) + from;
+        else
+            return -1;
+    }
+
+    public static void main(String[] args) {
+        int num1 = getRandomNumber(SHIFT, MAX_INT);
+        int num2 = getRandomNumber(SHIFT, MAX_INT);
+        int num3 = getRandomNumber(SHIFT, MAX_INT);
+        int concatenatedNum1Num2 = concatenateNumbers(num1, num2);
+
+        out.println(num1);
+        out.println(num2);
+        out.println(num3);
+        out.println(concatenatedNum1Num2 + " - " + num3 + " = " + (concatenatedNum1Num2 - num3));
     }
 }
