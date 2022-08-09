f19139a9cc34948e23d3699f246ef32b8ff41f8e
alexsnitol <sslotin74@gmail.com>
2022-08-09 13:15:51 +0300

    deleted unused imports

--- autoserviceAdmin/src/main/java/ru/senla/autoservice/model/User.java
+++ autoserviceAdmin/src/main/java/ru/senla/autoservice/model/User.java
@@ -1,8 +1,6 @@
 package ru.senla.autoservice.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
